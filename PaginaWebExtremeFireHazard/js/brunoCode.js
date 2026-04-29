4dbe148a33c8d285570ce84423321eae5ecf2a85
Insideab <heynoobz4@gmail.com>
2026-04-29 19:09:24 +0200

    Cositas de maquetación

---
+++ PaginaWebExtremeFireHazard/js/brunoCode.js
@@ -0,0 +1,21 @@
+
+
+function LowerTitle()
+{
+	var title = document.getElementById('header');
+	title.style.paddingTop = '170px';
+	title.style.paddingBottom = '45px';
+	
+	var logo = document.getElementById('logo');
+	logo.style.height = '120px'
+}
+
+function RiseTitle()
+{
+	var title = document.getElementById('header');
+	title.style.paddingTop = '110px';
+	title.style.paddingBottom = '15px';
+	
+	var logo = document.getElementById('logo');
+	logo.style.height = '90px'
+}
