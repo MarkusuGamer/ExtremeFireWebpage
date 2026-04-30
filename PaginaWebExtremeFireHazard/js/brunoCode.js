925c04824339c9f692933f5717308ae6fb85b05a
Insideab <heynoobz4@gmail.com>
2026-04-30 17:54:56 +0200

    Imagen que cambia + Botón de descargar build

--- PaginaWebExtremeFireHazard/js/brunoCode.js
+++ PaginaWebExtremeFireHazard/js/brunoCode.js
@@ -1,21 +1,73 @@
 
+var navBar; //Referencia a la barra de navegación
+var title; //referencia al título
+var logo; //Referencia al logo de la navbar (el icono de EFH con el fueguito)
 
-function LowerTitle()
+var images = ["../media/img/Marcos/IrusDawn.png", 
+				"../media/img/Bruno/Iru_1.webp",
+				"../media/img/Bruno/Iru_2.webp",
+				"../media/img/Bruno/Iru_3.webp",
+				"../media/img/Bruno/Iru_4.webp"];
+				
+var currentImg = 0;
+var display;
+
+function Start()
 {
-	var title = document.getElementById('header');
+	navbar = document.getElementById('nav'); //Referencia a la barra de navegación
+    title = document.getElementById('header'); //referencia al título
+    logo = document.getElementById('logo'); //Referencia al logo (icono EFH)
+    display = document.getElementById('irusDawnCover'); //Referencia a la foto principal
+    
+    display.addEventListener('animationend', () => {display.classList.remove('shakeAnim');});  
+    
+    images.forEach(src => {var img = new Image(); img.src = src;});  
+}  
+
+function navScrollHide() //Esta función se encarga de ocultar la barra de navegación cuando scrolleas hacia abajo
+{
+    if (window.scrollY > 50) //si tu variable de scroll vertical es mínima
+    {
+        navbar.classList.add('scrolled'); //Se añade esta clase que oculta la navbar.
+    } 
+	else 
+	{
+        navbar.classList.remove('scrolled'); //Si no, se quita la clase para que vuelva a aparecer.
+    }
+}
+
+function NextImage() //Hace que el source de la imágen principal cambie (mostrando distintas imágenes del juego).
+{
+	 console.log("NextImage ejecutado", currentImg, display);
+	currentImg++; //Sube el index de la foto
+	if(currentImg > 4) currentImg = 0; //si el index ha llegado a su máximo, volver a 0 (cíclico)
+	display.src = images[currentImg]; //cambia el source de la foto a la que toque dependiendo del index.
+}	
+
+function LowerTitle() //Esta función baja el título cuando el navbar se hace grande (para dejar espacio)
+{
 	title.style.paddingTop = '170px';
 	title.style.paddingBottom = '45px';
-	
-	var logo = document.getElementById('logo');
 	logo.style.height = '120px'
 }
 
-function RiseTitle()
+function RiseTitle() //Esta función sube el título cuando el navbar se hace chiquito
 {
-	var title = document.getElementById('header');
 	title.style.paddingTop = '110px';
 	title.style.paddingBottom = '15px';
-	
-	var logo = document.getElementById('logo');
 	logo.style.height = '90px'
 }
+
+function ImageBounce() //Esta función se encarga de animar el rebote de la imagen central de la página
+{
+	var img = document.getElementById('irusDawnCover'); //Obtiene la imágen central de Iru's Dawn
+    img.classList.add("shakeAnim"); //Añade la clase a través de la cual se animan (css)
+    setTimeout(() => img.classList.remove("shakeAnim"), 500); //Se la quita tras un timeout de medio segundo
+}
+
+function GoToTop() //esta función hace que, al dar a un botón, puedas subir a la parte de arriba de la página automáticamente.
+{
+  document.body.scrollTop = 0; // Esto es para que lo pueda hacer en safari
+  document.documentElement.scrollTop = 0; // Esto es para que lo pueda hacer en Chrome, Opera y Firefox
+} 
+	
