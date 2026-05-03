9f9ea9528765e7b056b142edb532e3b473f92b15
pattysanz27 <168010336+pattysanz27@users.noreply.github.com>
2026-05-03 16:12:18 +0200

    necesito revisarme las gafas urgentemente

--- PaginaWebExtremeFireHazard/js/patriciaCode.js
+++ PaginaWebExtremeFireHazard/js/patriciaCode.js
@@ -1,20 +1,18 @@
-var navBar; //Variable para guardar la barra de navegación
+var navbar; //Variable para guardar la barra de navegación
 
-
-
-funtion Start() //Función que se ejecuta cuando la página acaba de cargar
+function Start() //Función que se ejecuta cuando la página acaba de cargar
 {
 	navbar = document.getElementById('nav');
 }
 
 function navScrollHide() //Función para ocultar la barra de navegación cuando el usuario scrollee para abajo
 {
-	if (window.scrolly > 10)//Si el usuario ha bajado más de 10 píxeles
+	if (window.scrollY > 10)//Si el usuario ha bajado más de 10 píxeles
 	{
 		navbar.classList.add('scrolled');
 	}
 	else
 	{
-		navbar.ClassList.remove('scrolled');
+		navbar.classList.remove('scrolled');
 	}
 }
